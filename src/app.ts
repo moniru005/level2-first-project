b50fe4603f5beec28ee063b60f739ce5b0dd9055
Monir <moniru005@gmail.com>
2024-05-19 22:52:26 +0600

    [DONE] Create Student Feature

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { StudentRoutes } from './app/modules/student/student.route'
 const app: Application = express()
 
 // parsers
@@ -7,10 +8,14 @@ const app: Application = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10
+// application routes
+app.use('/api/v1/students', StudentRoutes)
 
+const getAController = (req: Request, res: Response) => {
+  const a = 10
   res.send(a)
-})
+}
+
+app.get('/', getAController)
 
 export default app
